eca57381e8abd489397bd8d8d2b05f8fdffce682
wolfreak99 <jbminor1991@gmail.com>
2017-08-09 16:15:50 -0400

    Create GenerateFlags

---
+++ ScenarioGenerator/GenerateFlags.cs
@@ -0,0 +1,13 @@
+﻿namespace ScenarioGenerator
+{
+	[System.Flags]
+	public enum GenerateFlags
+	{
+		None = 0,
+		Height = 1 << 0,
+		TerrainType = 1 << 1,
+		Water = 1 << 2,
+		Trees = 1 << 3,
+		All = Height | TerrainType | Water | Trees
+	}
+}
